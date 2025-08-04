e09850a0013f0c3a7b0a609aaf6c5e76e3438508
quangkhaihot <quangkhai692004@gmail.com>
2025-08-04 13:34:00 +0700

    first commit

---
+++ src/locales/languages/vn.ts
@@ -0,0 +1,30 @@
+import type { Translation } from "../translation";
+import key from "../keys";
+
+export const vn: Translation = {
+  [key.copy_right_author]: "Tác giả",
+  [key.copy_right_publish_date]: "Ngày xuất bản",
+  [key.copy_right_license]: "Giấy phép",
+
+  [key.nav_bar_home]: "Trang chủ",
+  [key.nav_bar_archive]: "Lưu trữ",
+  [key.nav_bar_about]: "Giới thiệu",
+  [key.nav_bar_github]: "GitHub",
+  [key.nav_bar_search_placeholder]: "Tìm kiếm",
+
+  [key.post_card_words]: "Từ",
+  [key.post_card_minutes]: "Phút",
+
+  [key.side_bar_categories]: "Danh mục",
+  [key.side_bar_tags]: "Thẻ",
+  [key.side_bar_view_more]: "Xem thêm",
+
+  [key.archive_year_title_count]: "Tổng cộng {{}} bài viết",
+
+  [key.pages_categories_archive]: "Lưu trữ theo danh mục",
+  [key.pages_tags_archive]: "Lưu trữ theo thẻ",
+  [key.pages_archive_archive]: "Lưu trữ",
+
+  [key.pages_tags_title]: "Thẻ",
+  [key.pages_categories_title]: "Danh mục",
+};
---
+++ yukina.config.ts
@@ -0,0 +1,64 @@
+import I18nKeys from "./src/locales/keys";
+import type { Configuration } from "./src/types/config";
+
+const YukinaConfig: Configuration = {
+  title: "Sonik",
+  subTitle: "Welcome, everyone, to my concert.",
+  brandTitle: "Sonik",
+
+  description: "Profile Site",
+
+  site: "https://quangkhaihot.github.io",
+
+  locale: "vn", // set for website language and date format
+
+  navigators: [
+    {
+      nameKey: I18nKeys.nav_bar_home,
+      href: "/",
+    },
+    {
+      nameKey: I18nKeys.nav_bar_archive,
+      href: "/archive",
+    },
+    {
+      nameKey: I18nKeys.nav_bar_about,
+      href: "/about",
+    },
+    {
+      nameKey: I18nKeys.nav_bar_github,
+      href: "https://github.com/WhitePaper233/yukina",
+    },
+  ],
+
+  username: "Quang Khải (Sonik)",
+  sign: "",
+  avatarUrl: "/public/avt.JPG",
+  socialLinks: [
+    {
+      icon: "line-md:github-loop",
+      link: "https://github.com/QuangKhaiHOT",
+    },
+  ],
+  maxSidebarCategoryChip: 6, // It is recommended to set it to a common multiple of 2 and 3
+  maxSidebarTagChip: 12,
+  maxFooterCategoryChip: 6,
+  maxFooterTagChip: 24,
+
+  banners: [  
+    "/public/banner/banner1.jpg",
+    "/public/banner/banner2.jpg",
+  ],
+
+  slugMode: "HASH", // 'RAW' | 'HASH'
+
+  license: {
+    name: "CC BY-NC-SA 4.0",
+    url: "https://creativecommons.org/licenses/by-nc-sa/4.0/",
+  },
+
+  // WIP functions
+  bannerStyle: "LOOP", // 'loop' | 'static' | 'hidden'
+};
+
+export default YukinaConfig;
